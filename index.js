fb2565c4a83f16238529ed6deb317e7f48b97265
DicksonNdumia <ndumiadickson19@gmail.com>
2026-04-11 08:11:53 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hello World! Welcome to docker❤️❤️❤️");
+function sum(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+console.log(subtract(2000, 1700));
+console.log(sum(1, 17));
